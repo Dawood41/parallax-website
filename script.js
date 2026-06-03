d7486732942d60a4ea3e6a82e62fc017da53e87f
Dawood Shah <your-github-email@gmail.com>
2026-06-03 14:03:24 +0500

    fix text animation

--- script.js
+++ script.js
@@ -41,9 +41,8 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('visible');
-        } else {
-            entry.target.classList.remove('visible');
-        }
+         }  
+
     });
 }, {
     threshold: 0.1
